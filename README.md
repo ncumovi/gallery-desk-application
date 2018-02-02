29746d54f60f4e71755452f1a6891187f882bb65
movi <451299572@qq.com>
2018-02-02 10:38:12 +0800

    画廊-桌面应用基础骨架

---
+++ README.md
@@ -0,0 +1,45 @@
+# vue-gallery
+
+> A gallery based on vue
+
+## Build Setup
+
+### install dependencies
+> npm install
+
+###  serve with hot reload at localhost:8080
+> npm run dev
+
+###  build for production with minification
+> npm run build
+
+###  build for production and view the bundle analyzer report
+> npm run build --report
+
+## Gallery By React
+
+one gallery project based on react
+
+使用 *vue2.x* 构架的图片画廊应用-参照慕课网《React实战》课程对应的画廊应用实战项目 
+
+## Demo & Examples
+
+Live demo: https://ncumovi.github.io/gallery-react/
+
+
+## Guideline
+
+icon文件引用的是阿里巴巴矢量图库 
+
+*link rel="stylesheet" href="//at.alicdn.com/t/font_510621_gkv0t0kevfs9k9.css"*
+
+## thinking
+
+  相比react的全程js操作（包括传递方法到子组件以及行内样式和class的相关传递和写法，基本都是字符串拼接的），个人觉得vue使用起来更得心应手。
+  组件化开发，vue生命周期、数据传递等，用起来更方便。不过可能是因为我很少使用react，基本在平时开发工作中都是用vue的。
+  vue也有一堆的坑，就拿数据传递来说吧，明明props上面有值，但是在template里面使用的时候却报错。
+  可能是我生命周期理解不到位，使用不当，最后用了黑魔法--延时器解决了问题，尴尬ing...
+
+## 求赞
+
+你的赞是我升职加薪，迎娶白富美，走上人生巅峰的基石和动力。
---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div  class='orention' v-if='show_orention'>
+      为了更好的体验效果，敬请横屏浏览哦！！
+    </div>
+    <router-view v-if='!show_orention' />
+  </div>
+</template>
+
+<script>
+// var Vc = require('vconsole')
+export default {
+  name: 'app',
+  mounted:function(){
+    var that = this;
+    // var vConsole = new Vc();
+    if (window.orientation === 180 || window.orientation === 0) {
+      that.show_orention = true; //竖屏显示
+    }
+    //移动端的浏览器一般都支持window.orientation这个参数，通过这个参数可以判断出手机是处在横屏还是竖屏状态。
+    window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
+      if (window.orientation === 180 || window.orientation === 0) {
+        that.show_orention = true; //竖屏显示
+      }
+      if (window.orientation === 90 || window.orientation === -90 ){
+        that.show_orention = false;
+      }
+    }, false);
+  },
+  data(){
+    return{
+      show_orention:false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+html,body{
+  width: 100%;
+  height: 100%;
+  background-color: #222;
+  margin: 0;
+}
+.orention{
+  width: 100%;
+  height: 100%;
+  z-index: 99;
+  font-size: 20px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  color:#fff;
+
+}
+</style>
