3e3d25629373c8c1acf9f2e3d474f1814eb8f9da
movi <451299572@qq.com>
2018-02-05 18:05:20 +0800

    grunt 压缩图片

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 var grunt = require('grunt');
 
+
 //进行配置
 grunt.config.init({
   pkg:grunt.file.readJSON('package.json'),
@@ -13,13 +14,30 @@ grunt.config.init({
       loadingGif:"./src/assets/install.gif",
       setupIcon:"./src/assets/favicon.ico"
     }
-  }
+  },
+  imagemin: {
+    /* 压缩图片大小 */
+    dist: {
+        options: {
+            optimizationLevel: 3 //定义 PNG 图片优化水平
+        },
+        files: [
+            {
+              expand: true,
+              cwd: 'originImg/',
+              src: ['**/*.{png,jpg,jpeg}'], // 优化 img 目录下所有 png/jpg/jpeg 图片
+              dest: 'static/' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
+            }
+        ]
+        }
+    },
 })
 
 //加载任务
 grunt.loadNpmTasks('grunt-electron-installer')
-
-//设置默认任务
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+//设置默认任务 注册任务
 grunt.registerTask('default',['create-windows-installer'])
+grunt.registerTask('img', ['imagemin']);
 
-grunt.tasks()
+
